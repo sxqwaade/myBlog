99ff7c2aa01b8192c218f44735148f08f267af0d
sxqwaade <sxqwade@sina.com>
2015-11-28 01:17:31 +0800

    refs little change

--- public/admin/routes.js
+++ public/admin/routes.js
@@ -5,7 +5,7 @@ define(['app', 'controllers/admin','controllers/aboutme','controllers/blog','con
             $routeProvider.
                 when('/aboutme', {templateUrl:'/partials/admin/aboutme.html',controller:aboutme}).
                 when('/blog', {templateUrl:'/partials/admin/blog.html',controller:blog}).
-                when('/login',{templateUrl:'/partials/admin/login.html',controller:login}).
+                when('/login',{templateUrl:'/partials/login.html',controller:login}).
                 otherwise({redirectTo:'/'});
         }]);
     });
